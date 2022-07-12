9558f6e10fe8d2ac2636fce892ca0daaadb54b51
Tamipes <tamipesagniyah@gmail.com>
2022-07-12 07:19:20 +0200

    Add the option to load a working kitty theme. Updated the README.md. And added tmux.conf .

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# dotfiles
-My dotflies
+# Tami's dotfiles
+This repository contains my dotflies in an unorderly fashion. Some applications have folders, some only have *".conf"* files.
+
+## **Don't run start.fish**
+The script deletes your dotfiles or even whole folders and simlinks the ones in this repo. And the symlinks are **not configured** to work with different folder setups.
--- start.fish
+++ start.fish
@@ -1,20 +1,27 @@
 #!/usr/bin/fish
 echo "This script will overwrite the folders as well. Don't run it if you arent sure what your doing."
 
+# This is the directory that the repo is in. So this script is at $HOME/.tamipes/start.fish
+set -l dir "$HOME/.tamipes"
 
 # Fastfetch
 sudo rm /usr/share/fastfetch/presets/tami
-sudo ln -s $HOME/.tamipes/fastfetch_presets/tami /usr/share/fastfetch/presets/tami
+sudo ln -s $dir/fastfetch_presets/tami /usr/share/fastfetch/presets/tami
 
 # Kitty
 rm $HOME/.config/kitty -r
-ln -s $HOME/.tamipes/kitty $HOME/.config/
+ln -s $dir/kitty $HOME/.config/
 
 
 # Ricemood
 rm $HOME/.config/ricemood -r
-ln -s $HOME/.tamipes/ricemood $HOME/.config/
+ln -s $dir/ricemood $HOME/.config/
 
 # Fish
 rm $HOME/.config/fish -r 
-ln -s $HOME/.tamipes/fish $HOME/.config/
+ln -s $dir/fish $HOME/.config/
+
+# Tmux
+rm $HOME/.config/tmux/tmux.conf
+ln -s $dir/tmux.conf $HOME/.config/tmux/tmux.conf
+
