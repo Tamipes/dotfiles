81c8e0e07f38982c9595f09798648d6450312e53
Tami <72661629+Tamipes@users.noreply.github.com>
2023-03-17 00:02:47 +0100

    Update start.sh

--- start.fish
+++ start.fish
@@ -1,47 +1,46 @@
-#!/usr/bin/fish
-echo "This script will overwrite the folders as well. Don't run it if you arent sure what your doing."
+#!/bin/bash
+echo "This script will overwrite the folders as well. Don't run it if you aren't sure what you're doing."
 
-# This is the directory that the repo is in. So this script is at $HOME/.tamipes/start.fish
-set -l dir "$HOME/.tamipes"
+# This is the directory that the repo is in. So this script is at $HOME/.tamipes/start.sh
+dir="$(dirname "$(readlink -f "$0")")"
 
 # Fastfetch
 sudo rm /usr/share/fastfetch/presets/tami
 sudo ln -s $dir/fastfetch_presets/tami /usr/share/fastfetch/presets/tami
 
 # Create .config file for later use.
-mkdir $HOME/.config
+mkdir -p $HOME/.config
 
 # Kitty
-rm $HOME/.config/kitty -r
+rm -r $HOME/.config/kitty
 ln -s $dir/kitty $HOME/.config/
 
-
 # Ricemood
-rm $HOME/.config/ricemood -r
+rm -r $HOME/.config/ricemood
 ln -s $dir/ricemood $HOME/.config/
 
 # Fish
-rm $HOME/.config/fish -r 
+rm -r $HOME/.config/fish
 ln -s $dir/fish $HOME/.config/
 
 # Tmux
-mkdir $HOME/.config/tmux
+mkdir -p $HOME/.config/tmux
 rm $HOME/.config/tmux/tmux.conf
 ln -s $dir/tmux.conf $HOME/.config/tmux/tmux.conf
 
 # Helix
 rm $HOME/.config/helix/config.toml
-mkdir $HOME/.config/helix
+mkdir -p $HOME/.config/helix
 ln -s $dir/helix/config.toml $HOME/.config/helix/config.toml
 
 # Cmus
 rm $HOME/.config/cmus/autosave
-mkdir $HOME/.config/cmus
+mkdir -p $HOME/.config/cmus
 ln -s $dir/cmus/autosave $HOME/.config/cmus/autosave
 
 # i3
 rm $HOME/.config/i3/config
-mkdir $HOME/.config/i3
+mkdir -p $HOME/.config/i3
 ln -s $dir/i3/config $HOME/.config/i3/config
 
 # starship
