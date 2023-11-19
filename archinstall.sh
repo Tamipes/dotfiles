472a1d811cfed9d5bf8649b3f32805a5b8b1b789
Tamipes <tamipesagniyah@gmail.com>
2023-11-19 01:55:40 +0100

    Some notes for installing arch, mainly which pakeges to use

---
+++ archinstall.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+
+# audio:
+#  ALSA(included)
+#  PulseAudio
+#    pulsemixer
+
+# basic:
+#  i3 https://gist.github.com/fjpalacios/441f2f6d27f25ee238b9bfcb068865db
+#    xorg-server 
+#    xorg-apps? 
+#    xorg-xinit
+#  ly
+#  polybar
+#  rofi
+#  kitty
+#  helix
+
+# AUR
+#  paru
+#    bat
