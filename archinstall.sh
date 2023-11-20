7c5a8497f46b15577504de02658903e830d77ea3
Tamipes <tamipesagniyah@gmail.com>
2023-11-20 13:45:15 +0000

    Archinstall added driver note

--- archinstall.sh
+++ archinstall.sh
@@ -10,6 +10,8 @@
 #  fsdostools
 
 # basic:
+#  Drivers:
+#   xf86-video-amdgpu xf86-video-intel xf86-video-nouveau xf86-video-vesa xf86-video-ati
 #  i3 https://gist.github.com/fjpalacios/441f2f6d27f25ee238b9bfcb068865db
 #    xorg-server 
 #    xorg-apps? 
